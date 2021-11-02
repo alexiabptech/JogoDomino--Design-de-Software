4397851528b42440877d9ea4b7ce1476356dfd8a
Alexia <81188486+alexiabptech@users.noreply.github.com>
2021-11-02 18:14:32 -0300

    Adiciona na mesa as peças que o jogador quer jogar

--- main.py
+++ main.py
@@ -58,3 +58,25 @@ def posicoes_possiveis(mesa, pecas):
                 posiveis_pos.append(i)
             i += 1    
     return posiveis_pos    
+
+#Adicionando peças a mesa, anaalisa as peças já em jogo e adiciona a peça que tá na mão do jogador
+def adiciona_na_mesa (peca, mesa):
+    print(mesa, peca)
+    if mesa == []:
+        mesa.append(peca)
+
+    elif peca[1] ==mesa[0][0]:
+        mesa.insert(0,peca)
+    
+    elif peca[0] == mesa[0][0]:
+        peca.reverse()
+        mesa.insert(0,peca) 
+
+    elif peca[1] == mesa[-1][1]:
+        peca.reverse()
+        mesa.append(peca) 
+
+    elif peca[0] == mesa[-1][1]:
+        mesa.append(peca)
+
+    return mesa  
