675b20d847ab9182e1fa6f74cb700e30aae54b59
Alexia <81188486+alexiabptech@users.noreply.github.com>
2021-11-03 15:01:02 -0300

    Juntando todas as funções num arquivo só

--- quem_ganhou.py
+++
@@ -1,8 +0,0 @@
-def verifica_ganhador(num_jog):
-    #num_jogadores = {numero: lista_peças}
-    #return posicao do jogador sem peças
-    for jogador, pecas in num_jog.items():
-        if pecas == []:
-            return jogador
-        
-    return -1
--- soma_pecas.py
+++
@@ -1,7 +0,0 @@
-def soma_pecas (l_pecas_jogador):
-
-    soma = 0
-    for peca in l_pecas_jogador:
-        for num in peca:
-            soma += num
-    return soma       
