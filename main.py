6278f2d9db370bd55295b671495c617911829c2c
Alexia <81188486+alexiabptech@users.noreply.github.com>
2021-11-05 18:46:01 -0300

    att

--- main.py
+++ main.py
@@ -5,23 +5,22 @@ while jogar == 'sim':
     print('Olá, bem-vindo ao Jogo Dominó!')
     #Define-se o numero
     num_jog = int(input('Qual a quantidade de participantes?: '))
-    print(num_jog)
 
     #Aqui, estamos pedindo pra que printe, de acordo com a nossa função  cria_pecas, pra que mostre a atd de peças disponiveis para a devida quantidade de jogadores
     #Aqui, cada jogador recebe 7 peças, aí as peças remanescentes vão para o monte
     lista_pecas = domino.cria_pecas()
     divisao_pecas = domino.inicia_jogo(num_jog, lista_pecas)
     print(divisao_pecas)
-
-
-    sorteia_jogador = random.randint(0,num_jog-1)
+    jogando = True
+    while jogando:
+        sorteia_jogador = random.randint(0,num_jog-1)
         #Inicia o jogo:
         #Para iniciar o jogo, temos que mostrar as peças que estão 
         # dispostas para o jogador
-    pecas = divisao_pecas['jogadores'][0] 
+        suas_pecas = divisao_pecas['jogadores'][0] 
 
-    print('O jogador {} inicia o jogo.'.format(sorteia_jogador))
-    print('Suas peças são {}'.format(pecas))
-    if sorteia_jogador == 0:
-        print(pecas)
-    
+        print('O jogador {} inicia o jogo.'.format(sorteia_jogador))
+        print('Suas peças são {}'.format(suas_pecas))
+        if sorteia_jogador == 0:
+            print(suas_pecas)
+
