bf68678fec3bf178d8b8488c3ec7318fdc8bbee5
Alexia <81188486+alexiabptech@users.noreply.github.com>
2021-11-05 16:08:57 -0300

    Colocando loops

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import domino 
 print('Olá, bem-vindo ao Jogo Dominó!')
+#Define-se o numero
 num_jog = int(input('Qual a quantidade de participantes?: '))
 print(num_jog)
 
-
+#Aqui, estamos pedindo pra que printe, de acordo com a nossa função  cria_pecas, pra que mostre a atd de peças disponiveis para a devida quantidade de jogadores
+#Aqui, cada jogador recebe 7 peças, aí as peças remanescentes vão para o monte
 lista_pecas = domino.cria_pecas()
 qtd = domino.inicia_jogo(num_jog, lista_pecas)
-print(qtd)
+#print(qtd)
+
+#Faz um loop aq 
+if qtd 
+    #Inicia o jogo:
+    #Para iniciar o jogo, temos que mostrar as peças que estão 
+    # dispostas para o jogador
+    pecas = qtd['jogadores'][0]
+    # mesa = 
+    # posicoes = 
+
+print('Suas peças são: ', pecas)
+print('Posições possíveis',)
 
