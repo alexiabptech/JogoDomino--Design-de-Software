1827824ea777b3884058f9f10cc9dfd7f2da3537
Alexia <81188486+alexiabptech@users.noreply.github.com>
2021-11-07 20:38:53 -0300

    Código atualizado

--- main.py
+++ main.py
@@ -14,47 +14,45 @@ while iniciando == 'sim':
     jogador_da_vez = qual_inicia
     
     jogando = True
-
+    
     while jogando:
-        #print(jogador_da_vez)
+        
         print('MESA: ', mesa)
         print('\n')
-        if pecas_possiveis != []: # se voce tiver peças para jogar
+        if jogador_da_vez == 0: #Para voce jogar
+            print('Sua vez de jogar')
+            pecas_possiveis = domino.posicoes_possiveis(mesa, jogadores[jogador_da_vez]) # suas peças possiveis para jogar  
+            print('As peças que voce tem {}'.format(div_jogo['jogadores'][0]))  
+            pecas_disponiveis = [div_jogo['jogadores'][0][i] for i in pecas_possiveis]
+            print('As peças que voce pode jogar {}'.format(pecas_disponiveis))  
+            print('As posicoes das peças que voce pode jogar {}'.format(pecas_possiveis))     
+            
+            if pecas_possiveis != []: # se voce tiver peças para jogar
                 #print(pecas_possiveis)
-                peca_jogada = int(input('Escolha a peça a ser jogada: '))
-                if peca_jogada not in pecas_possiveis:
-                    print('Você não possui essa peça.')
-                    peca_jogada = int(input('Escolha outra peça válida: '))
-                    mesa = domino.adiciona_na_mesa(jogadores[jogador_da_vez][peca_jogada], mesa)
-                    jogadores[jogador_da_vez].pop(peca_jogada)
-                    print('Voce colocou a peça: ', peca_jogada)
-                    #del(jogadores[jogador_da_vez][peca_jogada])
-                    jogador_da_vez += 1   # para não jogar mais de uma vez
+                peca_jogada = int(input('Escolha a peça a ser jogada:(0/6) '))
+                mesa = domino.adiciona_na_mesa(jogadores[jogador_da_vez][peca_jogada], mesa)
+                jogadores[jogador_da_vez].pop(peca_jogada)
+                print('Voce colocou a peça: ', peca_jogada)
+                #del(jogadores[jogador_da_vez][peca_jogada])
+                jogador_da_vez += 1   # para não jogar mais de uma vez
+
+            else: # caso você não tenha peças para jogar
+               
+                if monte != []: # se tiver peça no monte eu eu pego uma
+                    while domino.posicoes_possiveis(mesa, jogadores[jogador_da_vez]) == [] and monte != []:
+                        jogadores[jogador_da_vez].append(monte[0])
+                        monte.pop(0)
+                    if domino.posicoes_possiveis(mesa, jogadores[jogador_da_vez]) != []:
+                        mesa = domino.adiciona_na_mesa(jogadores[jogador_da_vez][-1], mesa)
+                        print('Voce colocou a peça: ', peca_jogada)
+                        jogador_da_vez += 1
+                    elif monte == []:
+                        jogador_da_vez += 1
+                        print('Voce pulou a vez')
 
                 else:
-
-                    mesa = domino.adiciona_na_mesa(jogadores[jogador_da_vez][peca_jogada], mesa)
-                    jogadores[jogador_da_vez].pop(peca_jogada)
-                    print('Voce colocou a peça: ', peca_jogada)
-                    #del(jogadores[jogador_da_vez][peca_jogada])
-                    jogador_da_vez += 1   # para não jogar mais de uma vez
-        else: # caso você não tenha peças para jogar
-               
-            if monte != []: # se tiver peça no monte eu eu pego uma
-                while domino.posicoes_possiveis(mesa, jogadores[jogador_da_vez]) == [] and monte != []:
-                    jogadores[jogador_da_vez].append(monte[0])
-                    monte.pop(0)
-                if domino.posicoes_possiveis(mesa, jogadores[jogador_da_vez]) != []:
-                    mesa = domino.adiciona_na_mesa(jogadores[jogador_da_vez][-1], mesa)
-                    print('Voce colocou a peça: ', peca_jogada)
-                    jogador_da_vez += 1
-                elif monte == []:
                     jogador_da_vez += 1
                     print('Voce pulou a vez')
-
-            else:
-                jogador_da_vez += 1
-                print('Voce pulou a vez')
        
         else:
             pecas_possiveis = domino.posicoes_possiveis(mesa, jogadores[jogador_da_vez])
@@ -81,7 +79,7 @@ while iniciando == 'sim':
                 else:
                     jogador_da_vez += 1   
                     print('O jogador {} pulou a vez'.format(jogador_da_vez)) 
-
+        
         if domino.verifica_ganhador(jogadores) != -1:
             print(f'O jogador {jogador_da_vez} venceu')
             jogando = False
@@ -89,7 +87,8 @@ while iniciando == 'sim':
         if jogador_da_vez >= qtd_jogadores:
             jogador_da_vez = 0
         # else:
-        # jogador_da_vez += 1
+        #     jogador_da_vez += 1
     resposta = input("Quer continuar jogando? ")
     if resposta != 'sim':
-        print('Adeus :)')            
+        print('Adeus :)')
+
